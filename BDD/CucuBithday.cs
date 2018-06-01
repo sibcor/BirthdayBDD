3d1d31ea0191c8d0b50a23ee0ad4a2f160639146
Sibcor <bakuninivan@yandex.ru>
2018-06-01 09:41:25 +0700

    Установил SpecFlow для 2015

---
+++ BDD/CucuBithday.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist; 
+
+namespace BDD
+{
+    [Binding]
+    public class CucuBithday
+    {
+
+    }
+}
